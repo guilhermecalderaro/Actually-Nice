7d1743f1bd9de02cb4176e9c8fae6837ec0625b7
Guilherme Calderaro <guilhermecald96@gmail.com>
2026-02-10 14:42:12 -0300

    feat: bump version to 1.0.0 and update `@require` URL to use raw.githubusercontent.com.

--- actually-nice.user.js
+++ actually-nice.user.js
@@ -1,11 +1,11 @@
 // ==UserScript==
 // @name         Actually NICE – My Schedule Summary
-// @version      0.3.3
+// @version      1.0.0
 // @description  Builds a dropdown summary table in the main document.
 // @author       Guilherme Calderaro <guilhermecald96@gmail.com>
 // @source       https://github.com/guilhermecalderaro/Actually-Nice
 // @match        https://equiti-wfm.nicecloudsvc.com/wfm/webstation/my-schedule*
-// @require      https://github.com/guilhermecalderaro/Actually-Nice/raw/refs/heads/main/dist/actually-nice.min.js
+// @require      https://raw.githubusercontent.com/guilhermecalderaro/Actually-Nice/refs/heads/main/dist/actually-nice.min.js
 // @noframes
 // @grant        none
 // ==/UserScript==
